885e4cf6b003ddf0b03ca2fe95be706523933a36
Saud bin Mohammed <samaphp@users.noreply.github.com>
2022-02-17 12:57:01 +0300

    Create drupal-deploy-basic.sh

---
+++ drupal-deploy-basic.sh
@@ -0,0 +1,25 @@
+#/bin/sh
+###
+# Basic deploy script.
+#
+# Script usage:
+# $ ./drupal-deploy-basic.sh FOLDERNAME
+#
+# Suggested usage for non-production environments:
+# $ ./drupal-deploy-basic.sh PROJECT/ENVIRONMENT
+###
+
+echo "██████ DEPLOY"
+cd /var/www/html/$1
+
+#git checkout composer.json
+echo "Pulling changes from git repo .."
+git pull
+echo "Installing composer .."
+composer install
+./vendor/bin/drush cim -y
+./vendor/bin/drush cim -y # duplicated to fix configuration sequence issue.
+./vendor/bin/drush locale-check
+./vendor/bin/drush locale-update
+./vendor/bin/drush cr
+chown www-data:www-data -R /var/www/html/$1
