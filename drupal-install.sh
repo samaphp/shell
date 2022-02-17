df62a0581b793ca59d3e22d45abc1af0872ada29
Saud bin Mohammed <samaphp@users.noreply.github.com>
2022-02-17 12:48:50 +0300

    Create drupal-install.sh

---
+++ drupal-install.sh
@@ -0,0 +1,26 @@
+#/bin/sh
+#####
+# This script is meant to be used to install a new Drupal site that is used to run on multiple stages.
+# This will install the Drupal site and import the site structure from the configuration files.
+# We are using same UUID to import same configurations.
+#####
+
+echo "██████ Salam! Please take a rest this process will take around 5-7 minutes ██████"
+mkdir private -p
+composer install
+./vendor/bin/drush si minimal --db-url=mysql://USER:PASS@localhost:3306/$1 --site-name=automated -y --account-pass=admin -y
+./vendor/bin/drush cset system.site uuid UUID_HERE -y
+./vendor/bin/drush en form_mode_manager -y # optional
+./vendor/bin/drush en menu_link_content -y # optional
+./vendor/bin/drush en workflow -y # optional
+./vendor/bin/drush config-import sync -y
+./vendor/bin/drush config-import sync -y # duplicated to fix configuration sequence issue.
+./vendor/bin/drush locale-check
+./vendor/bin/drush locale-update
+./vendor/bin/drush cr
+echo "██████ Installation process has been finished successfully ██████"
+echo "██████ Use these credentials admin/admin or the following URL ██████"
+./vendor/bin/drush uli
+
+# Change the folders owner to the needful user:group.
+chown -R www-data:www-data ./*
