85d2ca750f912450eb8163b4d134d2bfde3c376f
Saud bin Mohammed <samaphp@users.noreply.github.com>
2022-05-20 18:04:52 +0300

    Create fedora-setup.sh

---
+++ fedora-setup.sh
@@ -0,0 +1,34 @@
+#/bin/sh
+# My Fedora setup script
+
+# Basic apps
+sudo dnf update
+sudo dnf install git -y
+sudo dnf install snapd -y
+sudo dnf install google-chrome -y
+sudo snap install slack --classic
+sudo snap install spotify
+sudo snap install phpstorm --classic
+sudo snap install code --classic
+
+# Docker installation
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# Lando
+wget https://files.lando.dev/installer/lando-x64-stable.rpm
+sudo dnf install lando-x64-stable.rpm -y
+sudo rm -rf lando-x64-stable.rpm
+
+# Albert
+sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:manuelschneid3r/Fedora_Rawhide/home:manuelschneid3r.repo
+sudo dnf install albert -y
+
+# NVIDIA Drivers
+# sudo dnf install akmod-nvidia
+## Get card model name: ` lspci | grep VGA `
+## Download NVIDIA: https://www.nvidia.com/Download/driverResults.aspx/187526/en-us
+## Install the Nvidia driver compilation dependencies with
+# sudo dnf install kernel-devel kernel-headers gcc make dkms acpid libglvnd-glx libglvnd-opengl libglvnd-devel pkgconfig
+# sudo bash NVIDIA-Linux-x86_64-510.68.02.run
