773556ad7e05b3273584bd42a124e6a3b06da8a6
Saud bin Mohammed <samaphp@users.noreply.github.com>
2022-08-11 00:53:03 +0300

    open mysql port for remote hosts

--- me-startup-script-mysql-server.sh
+++ me-startup-script-mysql-server.sh
@@ -10,3 +10,9 @@ apt update
 apt install mariadb-server
 mysql_secure_installation
 systemctl start mariadb.service
+
+# open port for all local machines
+sed -i "s/127.0.0.1/0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
+systemctl restart mariadb.service
+
+# netstat -ant | grep 3306
