d73b2889e11da13601040f40a1ce88e40befee5f
Saud bin Mohammed <samaphp@users.noreply.github.com>
2024-06-09 17:19:27 +0300

    streamline lando installation

--- linux-opensuse-setup.sh
+++ linux-opensuse-setup.sh
@@ -18,11 +18,11 @@ newgrp docker
 sudo systemctl restart docker
 
 # Lando
-#wget https://files.lando.dev/installer/lando-x64-stable.rpm
-#sudo dnf install lando-x64-stable.rpm -y
-#sudo rm -rf lando-x64-stable.rpm
-#sudo usermod -a -G docker $USER $ exit
-#lando --channel stable
+wget https://files.lando.dev/installer/lando-x64-stable.rpm
+sudo zypper --non-interactive install --allow-unsigned-rpm lando-x64-stable.rpm
+sudo rm -rf lando-x64-stable.rpm
+sudo usermod -a -G docker $USER
+lando --channel stable
 
 sudo sh -c 'echo "$USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers'
 
