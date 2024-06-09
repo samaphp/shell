b9c81d41351695d7c0627d884a9fe81399c3bca3
Saud bin Mohammed <samaphp@users.noreply.github.com>
2024-06-09 14:28:36 +0300

    enforce yes for app installations

--- linux-opensuse-setup.sh
+++ linux-opensuse-setup.sh
@@ -1,14 +1,14 @@
 #/bin/sh
 # My OpenSUSE setup script
 
-sudo flatpak install flathub com.google.Chrome
-sudo flatpak install flathub com.visualstudio.code
-sudo flatpak install flathub com.slack.Slack
-sudo flatpak install flathub io.github.brunofin.Cohesion
-sudo flatpak install flathub com.jetbrains.PhpStorm
-sudo flatpak install flathub com.getpostman.Postman
-sudo flatpak install flathub org.kde.kteatime
-#sudo flatpak install flathub com.github.devalien.workspaces
+sudo flatpak install flathub com.google.Chrome -y
+sudo flatpak install flathub com.visualstudio.code -y
+sudo flatpak install flathub com.slack.Slack -y
+sudo flatpak install flathub io.github.brunofin.Cohesion -y
+sudo flatpak install flathub com.jetbrains.PhpStorm -y
+sudo flatpak install flathub com.getpostman.Postman -y
+sudo flatpak install flathub org.kde.kteatime -y
+#sudo flatpak install flathub com.github.devalien.workspaces -y
 
 # docker
 zypper install docker docker-compose docker-compose-switch
