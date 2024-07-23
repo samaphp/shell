173bea6c804c53bb73677e15821e5ca307a63012
Saud bin Mohammed <samaphp@users.noreply.github.com>
2024-07-23 13:48:21 +0300

    add curl cli

--- ollama-test.sh
+++ ollama-test.sh
@@ -7,3 +7,30 @@ ollama pull mistral
 ollama pull openhermes
 
 # Now the open web ui is accessible on http://127.0.0.1:8080/ 
+
+
+
+
+# -------
+
+
+## curl cli calls
+
+## getting version (check connection)
+#curl -D - -s http://0.0.0.0:11434/api/version
+#curl http://0.0.0.0:11434/api/version
+
+## send simple chat and get a response
+#curl http://0.0.0.0:11434/v1/chat/completions -d '{
+#    "model": "mistral",
+#    "messages": [
+#        {
+#            "role": "system",
+#            "content": "You are a helpful assistant."
+#         },
+#         {
+#             "role": "user",
+#             "content": "Who are you?"
+#         }
+#     ]
+# }'
