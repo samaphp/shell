8f4170d7de31235b67cac23831de4d50109ed21d
Saud bin Mohammed <samaphp@users.noreply.github.com>
2024-07-29 12:45:30 +0300

    add albert libwidgetboxmodel

--- linux-opensuse-setup.sh
+++ linux-opensuse-setup.sh
@@ -38,6 +38,7 @@ sudo snapper create --description "first-snapshot"
 sudo zypper install albert
 sudo ln -s /usr/lib64/albert/libalbert.so /usr/lib64/libalbert.so
 sudo ln -s /usr/lib64/albert/libxdg.so /usr/lib64/libxdg.so
+sudo ln -s /usr/lib64/albert/plugins/libwidgetboxmodel.so /usr/lib64/libwidgetboxmodel.so
 # search for lib files: sudo find / -name "libalbert.so" 2>/dev/null
 
 # update pip
