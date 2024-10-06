7940ff90d8f07703641ffab222bd2b421b55f322
Saud bin Mohammed <samaphp@users.noreply.github.com>
2024-10-06 15:29:22 +0300

    Install Portainer

--- docker-machine.sh
+++ docker-machine.sh
@@ -2,6 +2,7 @@
 # Docker machine installation on Ubuntu server
 
 # Install Docker
+# Source: https://docs.docker.com/engine/install/ubuntu/
 # uninstall all conflicting packages
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
@@ -21,3 +22,11 @@ sudo apt-get update
 
 # Install Docker
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Install Portainer
+# Source: https://docs.portainer.io/2.22/start/install-ce/server/docker/linux
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.22.0
+# Restart portainer to make sure your can setup admin
+docker stop portainer
+docker start portainer
