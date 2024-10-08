c5080a8be98e1dc720ea258a2ee5e69e0bea94c8
Saud bin Mohammed <samaphp@users.noreply.github.com>
2024-10-08 07:59:55 +0300

    Create dev-neovim.sh

---
+++ dev-neovim.sh
@@ -0,0 +1,11 @@
+#/bin/sh
+# setup neovim
+sudo zypper install cmake
+
+git clone https://github.com/neovim/neovim.git
+cd neovim
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+mkdir ~/.config/nvim
+cd ~/.config/nvim
+wget https://raw.githubusercontent.com/nvim-lua/kickstart.nvim/refs/heads/master/init.lua
