ece89398f7858e993cf1c3000b1209765106afc9
Saud bin Mohammed <samaphp@users.noreply.github.com>
2024-10-31 12:33:47 +0300

    Create service-npm-pm2.sh

---
+++ service-npm-pm2.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+# Install node nvm
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+# install latest node
+nvm install node
+
+# Install pm2
+npm install pm2 -g
+
+# pm2 start "npm run start" --name=mysite
