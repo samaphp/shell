7244bb95762144b844d059952e23f2670091a6ac
Saud bin Mohammed <samaphp@users.noreply.github.com>
2024-11-28 11:11:56 +0300

    add comment for service status command

--- service-php.sh
+++ service-php.sh
@@ -6,4 +6,5 @@ sudo apt install -y software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update
 sudo apt install php8.2-{cli,fpm,mysql,common,xml,curl,gd,mbstring} -y
-sudo phpenmod mbstring curl xml
+sudo phpenmod mbstring curl XML
+#systemctl status php8.2-fpm.service
