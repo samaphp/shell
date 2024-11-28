87ac513bff1c971c8a67992f37723121916a61bc
Saud bin Mohammed <samaphp@users.noreply.github.com>
2024-11-28 14:21:38 +0300

    Create service-composer.sh

---
+++ service-composer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Script to install PHP composer.
+
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo mv composer.phar /usr/local/bin/composer
